5b5d79dcedc5bb4164556f3a246508e520a86e9f
贺天学 <htx17385903148@163.com>
2020-07-20 12:00:14 +0800

    VSDFD

---
+++ src/main/java/com/nfdw/test/util/TestJar.java
@@ -0,0 +1,70 @@
+package com.nfdw.test.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestJar {
+
+	public static void main(String[] args) {
+
+		List<Integer> list = new ArrayList<>();
+		// list.add(1);
+		if (0 != list.size()) {
+			System.out.println(2145253);
+		} else {
+			System.out.println(66666);
+		}
+
+//		Date date = new Date();// 获取今天的日期
+//		Calendar calendar = Calendar.getInstance();
+//
+//		calendar.setTime(date);
+//		calendar.add(Calendar.DAY_OF_MONTH, 1);
+//		Date tomday = calendar.getTime();
+//
+//		calendar.add(Calendar.DAY_OF_MONTH, +1);
+//		Date postnatal = calendar.getTime();
+//
+//		SimpleDateFormat sdf1 = new SimpleDateFormat("yyyyMMdd");// 显示20171027格式
+//		String tomDate = sdf1.format(tomday);
+//		String today = sdf1.format(date);
+//		String postnatals = sdf1.format(postnatal);
+//		System.out.println(tomDate);
+//		System.out.println(today);
+//		System.out.println(postnatals);
+
+		/**
+		 * Date date = new Date(); SimpleDateFormat sdf1 = new
+		 * SimpleDateFormat("yyyyMMdd");// 显示20171027格式
+		 * System.out.println(sdf1.format(date));
+		 * 
+		 * /** List<String> listId = new ArrayList<>(); listId.add("1");
+		 * listId.add("2"); listId.add("3"); listId.add("1"); listId.add("1"); if
+		 * (!listId.contains("366")) { System.out.println("匹配"); } else {
+		 * System.out.println("不匹配"); }
+		 * 
+		 * /* String idString = "20200201120000"; String trDate = "19990202120000"; if
+		 * ("20200201120000" > "19990202120000") {
+		 * 
+		 * }
+		 * 
+		 * Map<String, String> map = new HashMap<String, String>();
+		 * 
+		 * /* String str = "thinking in java"; int index = str.indexOf("java");
+		 * System.out.println("index:" + index); /* indexOf(String str,int index)
+		 * 查找给定字符串在当前字符串的位置 首先第一个参数要在当前字符串中找到 然后返回返回第一个字母所在的下标位置
+		 * 
+		 * index = str.indexOf("in", 5); System.out.println("index:" + index); index =
+		 * str.indexOf("in", 7); System.out.println("index:" + index); // 返回最后一个in出现的位置
+		 * index = str.lastIndexOf("in"); System.out.println("index:" + index);
+		 * 
+		 * // email @ String mail = "123123123.com"; index = mail.indexOf("@"); if
+		 * (index > 0 && index < mail.length() - 1) { System.out.println("是邮箱"); } else
+		 * { System.out.println("不是邮箱"); }
+		 */
+		/**
+		 * MyFristJar myFristJar = new MyFristJar(); myFristJar.myprint();
+		 */
+	}
+
+}
